04ef6b452bf2e00b1a38dc308fae204726ff9ff7
aadithpm <aadithpm@gmail.com>
2019-01-20 12:45:47 -0600

    Added solution for Intersection of Two Linked Lists

---
+++ Easy/IntersectionOfTwoLinkedLists.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+        i = headA
+        j = headB
+        while i != j and (i or j):
+            if not i:
+                i = headB
+            if not j:
+                j = headA
+            if i == j:
+                break
+            i = i.next
+            j = j.next
+        return i if i == j else None
