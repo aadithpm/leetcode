233487495f3b704c7de34d5fa4d363cdbd978fed
aadithpm <aadithpm@gmail.com>
2019-01-22 16:27:53 -0600

    Added solution for Rotate Array

---
+++ Easy/RotateArray.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverse(self, nums, start, end):
+        while start < end:
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start += 1
+            end -= 1
+            
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        # nums = nums[k + 1:len(nums)] + nums[0:k + 1]
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
