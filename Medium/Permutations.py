091e2097a4bf9c923f495ed461717f51167ead73
aadithpm <aadithpm@gmail.com>
2019-01-27 18:34:12 -0600

    Added solution for Permutations

---
+++ Medium/Permutations.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(start, end):
+            if start == end:
+                outp.append(nums[:])
+            for i in range(start, end):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1, end)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        outp = []
+        backtrack(0, len(nums))
+        return outp
