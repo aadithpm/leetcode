0c3b236b3c65124388dcbeef52fade8e4b884df9
Aadith PM <aadithpm@gmail.com>
2019-10-08 09:38:23 -0500

    Added Best Time To Buy And Sell Stock

---
+++ Sprint/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        minprice = float('inf')
+        maxprofit = 0
+        for price in prices:
+            if price < minprice:
+                minprice = price
+            elif price - minprice > maxprofit:
+                maxprofit = price - minprice
+        return maxprofit
