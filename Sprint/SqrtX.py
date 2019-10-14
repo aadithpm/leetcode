8e55a6e252c585479af4732936265948e15fc8c6
Aadith PM <aadithpm@gmail.com>
2019-10-14 11:55:32 -0500

    Added Sqrt(X)

---
+++ Sprint/SqrtX.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        
+        l, r = 0, x
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if mid * mid <= x < (mid + 1) * (mid + 1):
+                return mid
+            elif x < mid * mid:
+                r = mid
+            else:
+                l = mid + 1
