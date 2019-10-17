40a72002621b38a36e5f05ab2b62504592417656
Aadith PM <aadithpm@gmail.com>
2019-10-17 15:45:04 -0500

    Added Subsets

---
+++ Sprint/Subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(subsets, temp, nums, start):
+            subsets.append(temp[:])
+            for i in range(start, len(nums)):
+                temp.append(nums[i])
+                backtrack(subsets, temp, nums, i + 1)
+                temp.pop()
+                
+        subsets = []
+        backtrack(subsets, [], nums, 0)
+
+        return subsets
+        
