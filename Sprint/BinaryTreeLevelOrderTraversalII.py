29b074725087ed527f484dd24fe79958b17b4517
aadithpm <aadithpm@gmail.com>
2019-10-27 11:11:10 -0500

    Added Binary Tree Level Order Traversal II

---
+++ Sprint/BinaryTreeLevelOrderTraversalII.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        stack = []
+        level = []
+        stack.append(root)
+        while stack:
+            level.append([node.val for node in stack])
+            this_level = []
+            for node in stack:
+                this_level.extend([node.left, node.right])
+            stack = [leaf for leaf in this_level if leaf]
+        return level[::-1]
